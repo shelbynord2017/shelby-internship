6e02c6d10f7e3d26fd0e155378630bd1077be15f
Shelbynord2017 <shelbynord2017@gmail.com>
2026-06-29 20:32:25 -0400

    starting top sellers

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  // starting section
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
