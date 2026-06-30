7e161180682b327900a3ec9edc2ac35cbe37ef80
Shelbynord2017 <shelbynord2017@gmail.com>
2026-06-29 23:26:44 -0400

    skeleton state

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import Skeleton from "../UI/Skeleton";
 
-const TopSellers = () => {
-  // starting section
+const TopSellers = ({ loading, topSellers = [] }) => {
+  
+  
   return (
+    
     <section id="section-popular" className="pb-5">
       <div className="container">
         <div className="row">
@@ -14,28 +17,56 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-12">
-            <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
+          {loading ? (
+            <>
+              {new Array(12).fill(0).map((_, i) => (
+              <div 
+              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ width: "100%", maxWidth: "100%", padding: "0" }}
+              key={i}
+              >
+                <div className="">
+                  <div className="lazy pp-author-skeleton">
+                    <Skeleton
+                      width="30px"
+                      height="30px"
+                      borderRadius="50%"
+                    />
+                    <i className="fa fa-check"></i>
                   </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                  <div className="author_list_info-skeleton">
+                    <Skeleton width="75px" height="10px" />
+                    <br />
+                    <Skeleton width="30px" height="10px" />
                   </div>
-                </li>
+                </div>
+              </div>
               ))}
-            </ol>
-          </div>
+            </>
+            ) : (
+            topSellers.map((seller, index) => (
+              <div className="col-md-12">
+                <ol className="author_list">
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to="/author">
+                        <img
+                          className="lazy pp-author"
+                          src={seller.authorImage || AuthorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to="/author">{seller.authorName}</Link>
+                      <span>{seller.price || 1.2} ETH</span>
+                    </div>
+                  </li>
+                </ol>
+              </div>
+              ))
+            )}
         </div>
       </div>
     </section>
