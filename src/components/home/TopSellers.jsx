c8dc703b716180e95db1a8b62f98ead1dc34c8f6
Shelbynord2017 <shelbynord2017@gmail.com>
2026-06-29 23:42:44 -0400

    skeleton adjust

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import { useParams } from "react-router-dom";
 import Skeleton from "../UI/Skeleton";
 
 const TopSellers = ({ loading, topSellers = [] }) => {
-  
-  
   return (
-    
     <section id="section-popular" className="pb-5">
       <div className="container">
         <div className="row">
@@ -20,34 +18,30 @@ const TopSellers = ({ loading, topSellers = [] }) => {
           {loading ? (
             <>
               {new Array(12).fill(0).map((_, i) => (
-              <div 
-              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              style={{ width: "100%", maxWidth: "100%", padding: "0" }}
-              key={i}
-              >
-                <div className="">
-                  <div className="lazy pp-author-skeleton">
-                    <Skeleton
-                      width="30px"
-                      height="30px"
-                      borderRadius="50%"
-                    />
-                    <i className="fa fa-check"></i>
-                  </div>
-                  <div className="author_list_info-skeleton">
-                    <Skeleton width="75px" height="10px" />
-                    <br />
-                    <Skeleton width="30px" height="10px" />
+                <div
+                  className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                  style={{ width: "100%", maxWidth: "100%", padding: "0" }}
+                  key={i}
+                >
+                  <div className="">
+                    <div className="lazy pp-author-skeleton">
+                      <Skeleton width="30px" height="30px" borderRadius="50%" />
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="author_list_info-skeleton">
+                      <Skeleton width="75px" height="10px" />
+                      <br />
+                      <Skeleton width="30px" height="10px" />
+                    </div>
                   </div>
                 </div>
-              </div>
               ))}
             </>
-            ) : (
-            topSellers.map((seller, index) => (
-              <div className="col-md-12">
-                <ol className="author_list">
-                  <li key={index}>
+          ) : (
+            <div className="col-md-12">
+              <ol className="author_list">
+                {topSellers.map((seller, index) => (
+                  <li>
                     <div className="author_list_pp">
                       <Link to="/author">
                         <img
@@ -63,10 +57,10 @@ const TopSellers = ({ loading, topSellers = [] }) => {
                       <span>{seller.price || 1.2} ETH</span>
                     </div>
                   </li>
-                </ol>
-              </div>
-              ))
-            )}
+                ))}
+              </ol>
+            </div>
+          )}
         </div>
       </div>
     </section>
